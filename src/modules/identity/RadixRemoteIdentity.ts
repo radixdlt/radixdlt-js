08085630b987cfabc6ee927a6e27c3515f886cbd
murraco <mauriurraco@gmail.com>
2018-11-25 18:29:09 -0300

    Add register method to RadixRemoteIdentity

--- src/modules/identity/RadixRemoteIdentity.ts
+++ src/modules/identity/RadixRemoteIdentity.ts
@@ -7,11 +7,13 @@ import RadixSerializer from '../serializer/RadixSerializer'
 import { RadixAtom, RadixKeyPair } from '../RadixAtomModel'
 
 export default class RadixRemoteIdentity extends RadixIdentity {
+    private TOKEN: string
     private REMOTE_URL: string
 
-    constructor(readonly keyPair: RadixKeyPair, host = 'localhost', port = '54345') {
+    constructor(readonly keyPair: RadixKeyPair, token, host = 'localhost', port = '54345') {
         super(keyPair)
 
+        this.TOKEN = token
         this.REMOTE_URL = `ws://${host}:${port}`
     }
 
@@ -23,7 +25,10 @@ export default class RadixRemoteIdentity extends RadixIdentity {
                 _socket.send(JSON.stringify({
                     jsonrpc: '2.0',
                     method: 'sign_atom',
-                    params: { atom: atom.toJson() },
+                    params: { 
+                        token: this.TOKEN,
+                        atom: atom.toJson(),
+                    },
                     id: 0,
                 }))
                 _socket.onmessage = (evt) => resolve(RadixSerializer.fromJson(JSON.parse(evt.data).result))
@@ -40,7 +45,10 @@ export default class RadixRemoteIdentity extends RadixIdentity {
                 _socket.send(JSON.stringify({
                     jsonrpc: '2.0',
                     method: 'decrypt_ecies_payload',
-                    params: { payload },
+                    params: {
+                        token: this.TOKEN,
+                        payload,
+                    },
                     id: 0,
                 }))
                 _socket.onmessage = (evt) => {
@@ -60,6 +68,26 @@ export default class RadixRemoteIdentity extends RadixIdentity {
         return Buffer.from(this.keyPair.keyPair.getPublic().encode('be', true))
     }
 
+    public static register(name: string, description: string, host = 'localhost', port = '54345') {
+        return new Promise<Buffer>((resolve, reject) => {
+            const _socket = new WebSocket(`ws:${host}:${port}`)
+            
+            _socket.onopen = () => {
+                _socket.send(JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'register',
+                    params: {
+                        name,
+                        description,
+                        permissions: ['sign_atom', 'decrypt_ecies_payload'],
+                    },
+                    id: 0,
+                }))
+                _socket.onmessage = (evt) => resolve(JSON.parse(evt.data).result)
+                _socket.onerror = (error) => reject(`WebSocket Error: ${JSON.stringify(error)}`)
+            }
+        })
+    }
     public static getRemotePublicKey(host = 'localhost', port = '54345') {
         return new Promise<Buffer>((resolve, reject) => {
             const _socket = new WebSocket(`ws:${host}:${port}`)
