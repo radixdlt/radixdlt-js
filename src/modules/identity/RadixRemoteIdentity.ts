61d0c316a4efb2f69059bfca65989d0868b3b900
murraco <mauriurraco@gmail.com>
2018-11-26 13:14:15 -0300

    Add JSDoc to RadixRemoteIdentity

--- src/modules/identity/RadixRemoteIdentity.ts
+++ src/modules/identity/RadixRemoteIdentity.ts
@@ -20,14 +20,26 @@ export default class RadixRemoteIdentity extends RadixIdentity {
         this.remoteUrl = remoteUrl
     }
 
-    private getSocketConnection(_socket: WebSocket) {
+    /**
+     * Checks if a given web socket connection is still alive, otherwise creates a new one
+     * 
+     * @param _socket - WebSocket connection
+     * @returns A WebSocket connection
+     */
+    private getSocketConnection(_socket: WebSocket): WebSocket {
         if (!_socket || _socket.readyState === _socket.CLOSING || _socket.readyState === _socket.CLOSED) {
             _socket = new WebSocket(this.remoteUrl)
         }
         return _socket
     }
 
-    public signAtom(atom: RadixAtom) {
+    /**
+     * Signs an atom with the wallet using the remote identity
+     * 
+     * @param atom - The atom to be signed
+     * @returns A promise with the signed atom
+     */
+    public signAtom(atom: RadixAtom): Promise<RadixAtom> {
         return new Promise<RadixAtom>((resolve, reject) => {
             const socket = this.getSocketConnection(this.signAtomSocket)
 
@@ -50,7 +62,13 @@ export default class RadixRemoteIdentity extends RadixIdentity {
         })
     }
 
-    public decryptECIESPayload(payload: Buffer) {
+    /**
+     * Decrypt the payload of an atom
+     * 
+     * @param payload - The payload of the atom to be decrypted
+     * @returns A promise with the decrypted payload
+     */
+    public decryptECIESPayload(payload: Buffer): Promise<Buffer> {
         return new Promise<Buffer>((resolve, reject) => {
             const socket = this.getSocketConnection(this.decryptECIESPayloadSocket)
 
@@ -77,18 +95,41 @@ export default class RadixRemoteIdentity extends RadixIdentity {
         })
     }
 
-    public getPublicKey() {
+    /**
+     * Returns the public key of this identity synchronously
+     * 
+     * @returns The public key of the identity
+     */
+    public getPublicKey(): Buffer {
         return Buffer.from(this.keyPair.keyPair.getPublic().encode('be', true))
     }
 
-    public static async createNew(name: string, description: string, host = 'localhost', port = '54345') {
+    /**
+     * Creates a new instance of a RadixRemoteIdentity
+     * 
+     * @param name - The name of the application that wants to use the remote identity
+     * @param description - The description of the application that wants to use the remote identity
+     * @param [host] - The host of the wallet
+     * @param [port] - The port in which the wallet server is being exposed
+     * @returns A promise with an instance of a RadixRemoteIdentity
+     */
+    public static async createNew(name: string, description: string, host = 'localhost', port = '54345'): Promise<RadixRemoteIdentity> {
         const publicKey = await RadixRemoteIdentity.getRemotePublicKey(host, port)
         const token = await RadixRemoteIdentity.register(name, description, host, port)
 
         return new RadixRemoteIdentity(RadixKeyPair.fromPublic(publicKey), token, `ws:${host}:${port}`)
     }
 
-    public static register(name: string, description: string, host = 'localhost', port = '54345') {
+    /**
+     * Registers a RadixRemoteIdentity to the wallet
+     * 
+     * @param name - The name of the application that wants to use the remote identity
+     * @param description - The description of the application that wants to use the remote identity
+     * @param [host] - The host of the wallet
+     * @param [port] - The port in which the wallet server is being exposed
+     * @returns A promise with a valid token to interact with the wallet
+     */
+    public static register(name: string, description: string, host = 'localhost', port = '54345'): Promise<string> {
         return new Promise<string>((resolve, reject) => {
             // This is an independant websocket because 'register' is a static method
             const socket = new WebSocket(`ws:${host}:${port}`)
@@ -110,7 +151,14 @@ export default class RadixRemoteIdentity extends RadixIdentity {
         })
     }
 
-    public static getRemotePublicKey(host = 'localhost', port = '54345') {
+    /**
+     * Returns the public key of this identity asynchronously
+     * 
+     * @param [host] - The host of the wallet
+     * @param [port] - The port in which the wallet server is being exposed
+     * @returns A promise with the public key of the identity
+     */
+    public static getRemotePublicKey(host = 'localhost', port = '54345'): Promise<Buffer> {
         return new Promise<Buffer>((resolve, reject) => {
             // This is an independant websocket because 'getRemotePublicKey' is a static method
             const socket = new WebSocket(`ws:${host}:${port}`)
