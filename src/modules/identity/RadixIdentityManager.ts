812d027a17f14db9926c39eb62ff7afe0f61a8c6
murraco <mauriurraco@gmail.com>
2018-11-26 13:31:56 -0300

    Add JSDoc to RadixIdentityManager

--- src/modules/identity/RadixIdentityManager.ts
+++ src/modules/identity/RadixIdentityManager.ts
@@ -9,6 +9,12 @@ import { RadixKeyPair } from '../RadixAtomModel'
 export default class RadixIdentityManager {
     public identities: TSMap<string, RadixIdentity> = new TSMap()
 
+    /**
+     * Generates a new RadixSimpleIdentity
+     * 
+     * @param keyPair - The key pair of the identity
+     * @returns An instance of a RadixSimpleIdentity
+     */
     public generateSimpleIdentity(): RadixIdentity {
         const keyPair = RadixKeyPair.generateNew()
         const identity = new RadixSimpleIdentity(keyPair)
@@ -18,6 +24,12 @@ export default class RadixIdentityManager {
         return identity
     }
 
+    /**
+     * Adds a new RadixSimpleIdentity
+     * 
+     * @param keyPair - The key pair of the identity
+     * @returns An instance of a RadixSimpleIdentity
+     */
     public addSimpleIdentity(keyPair: RadixKeyPair): RadixIdentity {
         const identity = new RadixSimpleIdentity(keyPair)
 
@@ -26,16 +38,32 @@ export default class RadixIdentityManager {
         return identity
     }
 
+    /**
+     * Generates a new RadixRemoteIdentity
+     * 
+     * @param name - The name of the application that wants to use the remote identity
+     * @param description - The description of the application that wants to use the remote identity
+     * @param [host] - The host of the wallet
+     * @param [port] - The port in which the wallet server is being exposed
+     * @returns A promise with an instance of a RadixRemoteIdentity
+     */
     public async generateRemoteIdentity(name: string, description: string, host = 'localhost', port = '54345'): Promise<RadixIdentity> {
-        const identity = await RadixRemoteIdentity.createNew(name, description)
-
-        return identity
+        return RadixRemoteIdentity.createNew(name, description)
     }
 
+    /**
+     * Adds a new RadixRemoteIdentity
+     * 
+     * @param name - The name of the application that wants to use the remote identity
+     * @param description - The description of the application that wants to use the remote identity
+     * @param [host] - The host of the wallet
+     * @param [port] - The port in which the wallet server is being exposed
+     * @returns A promise with an instance of a RadixRemoteIdentity 
+     */
     public async addRemoteIdentity(name: string, description: string, host = 'localhost', port = '54345'): Promise<RadixIdentity> {
         const identity = await RadixRemoteIdentity.createNew(name, description)
 
-        this.identities.set(RadixKeyPair.fromPublic(identity.getPublicKey()).getAddress(), identy)
+        this.identities.set(RadixKeyPair.fromPublic(identity.getPublicKey()).getAddress(), identity)
 
         return identity
     }
