b74f749ce3de58a76628e3d925f6d36602cdf5cc
murraco <mauriurraco@gmail.com>
2018-11-26 13:24:10 -0300

    Add remote identity management support to RadixIdentityManager

--- src/modules/identity/RadixIdentityManager.ts
+++ src/modules/identity/RadixIdentityManager.ts
@@ -2,23 +2,40 @@ import { TSMap } from 'typescript-map'
 
 import RadixIdentity from './RadixIdentity'
 import RadixSimpleIdentity from './RadixSimpleIdentity'
+import RadixRemoteIdentity from './RadixRemoteIdentity'
 
 import { RadixKeyPair } from '../RadixAtomModel'
 
 export default class RadixIdentityManager {
     public identities: TSMap<string, RadixIdentity> = new TSMap()
 
-    public generateSimpleIdentity() {
+    public generateSimpleIdentity(): RadixIdentity {
         const keyPair = RadixKeyPair.generateNew()
         const identity = new RadixSimpleIdentity(keyPair)
+
         this.identities.set(keyPair.getAddress(), identity)
 
         return identity
     }
 
-    public addSimpleIdentity(keyPair: RadixKeyPair) {
+    public addSimpleIdentity(keyPair: RadixKeyPair): RadixIdentity {
         const identity = new RadixSimpleIdentity(keyPair)
+
         this.identities.set(keyPair.getAddress(), identity)
+
+        return identity
+    }
+
+    public async generateRemoteIdentity(name: string, description: string, host = 'localhost', port = '54345'): Promise<RadixIdentity> {
+        const identity = await RadixRemoteIdentity.createNew(name, description)
+
+        return identity
+    }
+
+    public async addRemoteIdentity(name: string, description: string, host = 'localhost', port = '54345'): Promise<RadixIdentity> {
+        const identity = await RadixRemoteIdentity.createNew(name, description)
+
+        this.identities.set(RadixKeyPair.fromPublic(identity.getPublicKey()).getAddress(), identy)
 
         return identity
     }
