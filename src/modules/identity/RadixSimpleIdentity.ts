7efcf45ea472eb86f6796d9067898f4efafe46d1
László Urszuly <laszlo@radixdlt.com>
2019-01-09 12:24:23 +0000

    Move instantiation of seeded identity to manager
    This change moves the instantiation capability/responsibility of seeded
    RadixSimpleIdentity's from the class definition to the corresponding
    identity manager class instead. This is done to align with existing
    project patterns and code styles.

--- src/modules/identity/RadixSimpleIdentity.ts
+++ src/modules/identity/RadixSimpleIdentity.ts
@@ -2,24 +2,10 @@ import RadixECIES from '../crypto/RadixECIES'
 import RadixIdentity from './RadixIdentity'
 
 import { RadixAtom, RadixAddress } from '../atommodel'
-import { radixHash } from '../common/RadixUtil';
 
 export default class RadixSimpleIdentity extends RadixIdentity {
     constructor(readonly address: RadixAddress) {
         super(address)
-    }
-
-    /**
-     * Create an instance of a Radix simple identity from an arbitrary byte
-     * buffer. This could e.g. be a friendly name of an account, in which
-     * case it would be created as <code>Buffer.from('friendly name')</code>.
-     *
-     * @param seed Buffer seed for the address
-     * @returns a new Radix account. 
-     */
-    public static fromSeed(seed: Buffer) {
-        const hash = radixHash(seed)
-        return new RadixSimpleIdentity(RadixAddress.fromPrivate(hash))
     }
 
     public async signAtom(atom: RadixAtom) {
