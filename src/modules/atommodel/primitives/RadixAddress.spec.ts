79fcce983dc822e04ae7e066544eee97c9962a68
murraco <mauriurraco@gmail.com>
2019-01-17 19:15:36 -0300

    Fix typo in RadixAddress test descriptions

--- src/modules/atommodel/primitives/RadixAddress.spec.ts
+++ src/modules/atommodel/primitives/RadixAddress.spec.ts
@@ -5,20 +5,20 @@ import { radixHash } from '../../common/RadixUtil'
 
 describe('RadixAddress', () => {
 
-    it('fifferent addresses from random seed has different private keys', () => {
+    it('different addresses from random seed has different private keys', () => {
         const address1 = RadixAddress.generateNew()
         const address2 = RadixAddress.generateNew()
         expect(address2.keyPair.getPrivate('hex')).to.not.equal(address1.keyPair.getPrivate('hex'))
     })
 
-    it('fifferent addresses from same string seed has same private key', () => {
+    it('different addresses from same string seed has same private key', () => {
         const seed = 'abc'
         const address1 = RadixAddress.fromPrivate(radixHash(Buffer.from(seed)))
         const address2 = RadixAddress.fromPrivate(radixHash(Buffer.from(seed)))
         expect(address2.keyPair.getPrivate('hex')).to.equal(address1.keyPair.getPrivate('hex'))
     })
 
-    it('fifferent addresses from same int-array seed has same private key', () => {
+    it('different addresses from same int-array seed has same private key', () => {
         const seed = [0x62, 0x75, 0x66, 0x66, 0x65, 0x72]
         const address1 = RadixAddress.fromPrivate(radixHash(Buffer.from(seed)))
         const address2 = RadixAddress.fromPrivate(radixHash(Buffer.from(seed)))
