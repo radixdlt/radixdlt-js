024a188adc898575e52fc3dc3fd23ad295638cfc
murraco <mauriurraco@gmail.com>
2019-02-07 20:13:57 -0300

    Remove hardcoded token class creation

--- test/integration/TokenBalance.spec.ts
+++ test/integration/TokenBalance.spec.ts
@@ -37,13 +37,7 @@ describe('Creating Token Classes', () => {
   const identity1 = identityManager.generateSimpleIdentity()
   const identity2 = identityManager.generateSimpleIdentity()
 
-  const symbol = 'TBD'
-  const name = 'my token name'
-  const description = 'my token description'
-  const granularity = new BN(1)
-
-  const TBD_TOKENCLASS = new RadixTokenClass(identity1.address, symbol, name, description, granularity)
-  const TBD_URI = `/${identity1.account.getAddress()}/tokenclasses/${symbol}`
+  const TBD_URI = `/${identity1.account.getAddress()}/tokenclasses/TBD`
 
   before(async () => {
     // Check node is available
@@ -74,6 +68,10 @@ describe('Creating Token Classes', () => {
   it('Create a single issuance TBD token with account1', function (done) {
     this.timeout(50000)
 
+    const symbol = 'TBD'
+    const name = 'my token name'
+    const description = 'my token description'
+    const granularity = new BN(1)
     const amount = 500
 
     new RadixTransactionBuilder().createTokenSingleIssuance(
@@ -94,22 +92,26 @@ describe('Creating Token Classes', () => {
   it('Send 5 TBD token to account2 and check new increased balance', function (done) {
     this.timeout(50000)
 
-    RadixTransactionBuilder.createTransferAtom(
-      identity1.account,
-      identity2.account,
-      TBD_TOKENCLASS,
-      new Decimal(5),
-    )
-      .signAndSubmit(identity1)
-      .subscribe({
-        complete: () => done(),
-        next: state => {
-          if (state === 'STORED') {
-            expect(identity2.account.transferSystem.tokenUnitsBalance[TBD_URI].toString()).to.eq('5')
-          }
-        },
-        error: e => done(new Error(e)),
+    radixTokenManager.getTokenClass(TBD_URI)
+      .then(tbdTokenClass => {
+        RadixTransactionBuilder.createTransferAtom(
+          identity1.account,
+          identity2.account,
+          tbdTokenClass,
+          new Decimal(5),
+        )
+          .signAndSubmit(identity1)
+          .subscribe({
+            complete: () => done(),
+            next: state => {
+              if (state === 'STORED') {
+                expect(identity2.account.transferSystem.tokenUnitsBalance[TBD_URI].toString()).to.eq('5')
+              }
+            },
+            error: e => done(new Error(e)),
+          })
       })
+      .catch(error => done(new Error(error)))
   })
 
   it('Check that the balance in account1 has decreased after sending 5 TBD', function () {
