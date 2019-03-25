a43d1cd7ee97ade264fd796c73bd05c0e24d9e15
Edgars Nemše <edgars.nemse@gmail.com>
2019-03-25 10:51:21 +0000

    Remove unnecessary signature padding RLAU-1068

--- src/modules/atommodel/crypto/RadixECSignature.ts
+++ src/modules/atommodel/crypto/RadixECSignature.ts
@@ -10,20 +10,9 @@ export class RadixECSignature extends RadixSerializableObject {
     public s: RadixBytes
 
     public static fromEllasticSignature(ecSig) {
-        let r = ecSig.r.toArray()
-        let s = ecSig.s.toArray()
-
-        // Pad values
-        if (r[0] & 0x80) {
-            r = [0].concat(r)
-        }
-        if (s[0] & 0x80) {
-            s = [0].concat(s)
-        }
-
         const sig = new RadixECSignature()
-        sig.r = new RadixBytes(r)
-        sig.s = new RadixBytes(s)
+        sig.r = new RadixBytes(ecSig.r.toArray())
+        sig.s = new RadixBytes(ecSig.s.toArray())
 
         return sig
     }
