079256f12a8eda0d6992f50f7dc7e9ba6687e0e8
Edgars Nemše <edgars.nemse@gmail.com>
2019-06-10 11:20:24 +0100

    Fix security issues

--- src/modules/common/RadixLogger.ts
+++ src/modules/common/RadixLogger.ts
@@ -1,8 +1,8 @@
-import log from 'loglevel'
+import log, { LogLevelDesc } from 'loglevel'
 
 export default class RadixLogger {
   // 'trace', 'debug', 'info', 'warn', 'error'
-  public static setLevel(level: string): void {
+  public static setLevel(level: LogLevelDesc): void {
     log.setLevel(level)
   }
 }
