cec456f29c02333c7605d7d4f8495e6836781ad7
Alexander Wormbs <alexmedkex@gmail.com>
2020-04-22 00:23:08 +0100

    make LedgerApp connect on every new function call

--- src/modules/hardwarewallet/LedgerApp.ts
+++ src/modules/hardwarewallet/LedgerApp.ts
@@ -1,121 +1,99 @@
 // https://github.com/radixdlt/radixdlt-ledger-app/blob/improve/change_cosmos_to_radix/docs/APDUSPEC.md
 
-
 import { RadixAddress, RadixAtom } from '../atommodel'
-import { ReturnCode, Instruction, CLA, Device, SignPayloadType } from './types'
-import { sendApduMsg } from './HWWallet'
+import { ReturnCode, Instruction, CLA, SignPayloadType } from './types'
+import { sendApduMsg, openConnection } from './HWWallet'
 
 const CHUNK_SIZE = 250
 
 // HD Derivation Path: /0'/0/0
-const BIP44_PATH = '80000000' + '00000000' + '00000000'
+const BIP44_PATH = '80000002' + '00000001' + '00000003'
 
-export interface LedgerApp {
-    getPublicKey: () => Promise<any>
-    signAtom: (atom: RadixAtom, address: RadixAddress) => Promise<any>,
-    getVersion: () => Promise<any>,
-    getDeviceInfo: () => any
+export async function getPublicKey(): Promise<any> {
+    return await sendMessage(
+        Instruction.INS_GET_PUBLIC_KEY,
+        Buffer.from(BIP44_PATH, 'hex'),
+        1
+    )
 }
 
-export default (device: Device): LedgerApp => {
+export async function getVersion() {
+    return await sendMessage(
+        Instruction.INS_GET_VERSION,
+        Buffer.from(''),
+    )
+}
 
-    function sendMessage(
-        ins: Instruction,
-        data: Buffer,
-        p1: number = 0,
-        p2: number = 0,
-    ): Promise<any> {
-        const sendRadixAppMsg = sendApduMsg(ins, data, p1, p2, CLA)(handleError).bind(null, device)
-        return parseResponse(
-            sendRadixAppMsg,
-            ins
-        )
-    }
+export async function signAtom(atom: RadixAtom, address: RadixAddress): Promise<RadixAtom> {
+    // TODO max size of dson byte array should be 2048 bytes
 
-    return {
-        async getPublicKey(): Promise<any> {
-            /*
-            return await sendMessage(
-                Instruction.INS_GET_PUBLIC_KEY,
-                Buffer.from(BIP44_PATH, 'hex')
-            )*/
+    const payload = atom.toDSON()
+    const chunks = chunksFromPayload(BIP44_PATH, payload)
 
-            // Temporary mock return value
-            
-            return Promise.resolve({
-                returnCode: 9000,
-                publicKey: RadixAddress.generateNew().getPublic()
-            })
-            
-        },
+    // temporarily removed
+    /*
+    sendMessage(
+        Instruction.INS_SIGN_ATOM,
+        chunks[0],
+        SignPayloadType.INIT
+    )*/
 
-        async getVersion() {
-            return await sendMessage(
-                Instruction.INS_GET_VERSION,
-                Buffer.from(''),
-            )
-        },
+    let response
+    let payloadType = SignPayloadType.ADD
 
+    for (let i = 1; i < chunks.length; i += 1) {
+        if (i === chunks.length - 1) payloadType = SignPayloadType.LAST
 
-        async signAtom(atom: RadixAtom, address: RadixAddress): Promise<any> {
-            // TODO max size of dson byte array should be 2048 bytes
+        /*////// replacing temporarily
+        response = await sendMessage(
+            Instruction.INS_SIGN_ATOM,
+            chunks[i],
+            payloadType,
+        )*/
 
-            const payload = atom.toDSON()
-            const chunks = chunksFromPayload(BIP44_PATH, payload)
+        response = {
+            returnCode: ReturnCode.SUCCESS,
+            signature: address.sign(atom.getHash())
+        }
+        ///////////////////////
 
-            sendMessage(
-                Instruction.INS_SIGN_ATOM,
-                chunks[0],
-                SignPayloadType.INIT
-            )
-
-            let response
-            let payloadType = SignPayloadType.ADD
-
-            for (let i = 1; i < chunks.length; i += 1) {
-                if (i === chunks.length - 1) payloadType = SignPayloadType.LAST
-
-                /*////// replacing temporarily
-                response = await sendMessage(
-                    Instruction.INS_SIGN_ATOM,
-                    chunks[i],
-                    payloadType,
-                )*/
-
-                response = {
-                    returnCode: ReturnCode.SUCCESS,
-                    signature: address.sign(atom.getHash())
-                }
-                ///////////////////////
-
-                if (response.returnCode !== ReturnCode.SUCCESS) {
-                    throw handleError(response.returnCode)
-                }
-            }
-
-            const signatureId = address.getUID()
-
-            /* temporarily removed
-            const signature = RadixECSignature.fromEllasticSignature({
-                r: response.signature.slice(0, 32),
-                s: response.signature.slice(32, 65)
-            })
-            */
-
-            // remember to replace with signature when not mocking
-            atom.signatures = { [signatureId.toString()]: response.signature }
-
-            return atom
-        },
-
-        getDeviceInfo() {
-            if (!device) throw new Error('Connection not opened.')
-    
-            return device.device.getDeviceInfo()
+        if (response.returnCode !== ReturnCode.SUCCESS) {
+            throw handleError(response.returnCode)
         }
     }
+
+    const signatureId = address.getUID()
+
+    /* temporarily removed
+    const signature = RadixECSignature.fromEllasticSignature({
+        r: response.signature.slice(0, 32),
+        s: response.signature.slice(32, 65)
+    })
+    */
+
+    // remember to replace with signature when not mocking
+    atom.signatures = { [signatureId.toString()]: response.signature }
+
+    return atom
 }
 
+export async function getDeviceInfo() {
+    const device = await openConnection()
+    return device.device.getDeviceInfo()
+}
+
+async function sendMessage(
+    ins: Instruction,
+    data: Buffer,
+    p1: number = 0,
+    p2: number = 0,
+): Promise<any> {
+    const device = await openConnection()
+    return parseResponse(
+        sendApduMsg.bind(null, ins, data, p1, p2, CLA, handleError, device),
+        ins
+    )
+}
 
 async function parseResponse(fn: () => Promise<Buffer>, instruction: Instruction): Promise<any> {
     const response = await fn()
@@ -134,6 +112,7 @@ async function parseResponse(fn: () => Promise<Buffer>, instruction: Instruction
             break
         case Instruction.INS_SIGN_ATOM:
             generatedResponse = generateSignAtomResponse(response)
+            break
     }
 
     return {
@@ -175,18 +154,28 @@ function generateSignAtomResponse(response: Buffer) {
     }
 }
 
-function handleError(error: any): Error {
-    switch (error.statusCode) {
+function handleError(returnCode: number): { error: Error, returnCode: number } {
+    switch (returnCode) {
         case ReturnCode.SW_USER_REJECTED:
-            return new Error('User canceled operation.')
+            return {
+                error: new Error('User canceled operation.'),
+                returnCode
+            }
         case ReturnCode.CLA_NOT_SUPPORTED:
-            return new Error(
-                'App identifier (CLA) mismatch. Are you running the Radix app?',
-            )
+            return {
+                error: new Error('App identifier (CLA) mismatch. Are you running the Radix app?'),
+                returnCode
+            }
         case ReturnCode.INS_NOT_SUPPORTED:
-            return new Error('Instruction not supported by app.')
+            return {
+                error: new Error('Instruction not supported by app.'),
+                returnCode
+            }
     }
-    return new Error(`Unknown Ledger error: ${error.statusCode}`)
+    return {
+        error: new Error(`Unknown Ledger error: ${returnCode}`),
+        returnCode
+    }
 }
 
 function chunksFromPayload(path: string, payload: Buffer): Buffer[] {
