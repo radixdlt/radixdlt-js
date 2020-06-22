03414e275462df3d333af86d8bd7e6a6d16655b9
Alex Wormbs <alexander.wormbs@gmail.com>
2020-06-22 12:10:03 +0100

    refactor

--- src/modules/hardware-wallet/src/atomByteOffsetMetadata.ts
+++ src/modules/hardware-wallet/src/atomByteOffsetMetadata.ts
@@ -6,23 +6,17 @@ interface ByteInterval {
     byteCount: number
 }
 
+/*
+* Loops over the UP particles in an atom, and for each particle key/attribute
+* finds the byte interval in the DSON encoded atom and writes them to a byte array.
+*/
 export function cborByteOffsetsOfUpParticles(atom: RadixAtom): Buffer {
     const upParticles = atom.getParticlesOfSpin(RadixSpin.UP)
-
-    const byteOffsets = Buffer.alloc(112, 0)
+    const byteOffsets = Buffer.alloc(112)
     const bytes = []
 
     for (const particle of upParticles) {
-        let byteIntervalOfKey: (key: string) => ByteInterval
-
-        if (particle instanceof RadixTransferrableTokensParticle) {
-            byteIntervalOfKey = byteIntervalInAtom(particle, atom)
-        } else {
-            byteIntervalOfKey = (key: string) => {
-                if (key !== 'serializer') { return { startsAtByte: 0, byteCount: 0 } }
-                return byteIntervalInAtom(particle, atom)(key)
-            }
-        }
+        const byteIntervalOfKey = getByteIntervalFunction(particle, atom)
 
         for (const key of ['address', 'amount', 'serializer', 'tokenDefinitionReference']) {
             const interval = byteIntervalOfKey(key)
@@ -30,19 +24,45 @@ export function cborByteOffsetsOfUpParticles(atom: RadixAtom): Buffer {
             bytes.push(interval.byteCount)
         }
     }
-
     writeBytes(bytes, byteOffsets)
-
     return byteOffsets
 }
 
-const intervalByString = (needle: Buffer, hayStack: Buffer): ByteInterval => {
-    return {
-        startsAtByte: hayStack.toString('hex').indexOf(needle.toString('hex')) / 2,
-        byteCount: needle.length,
+/*
+* Get a byte interval function depending on the type of particle.
+*
+* For transferrable tokens particles, we use the regular byteIntervalInAtom method.
+* In other cases, we need to add a special check.
+*/
+const getByteIntervalFunction = (particle: RadixParticle, atom: RadixAtom): (key: string) => ByteInterval => {
+    if (particle instanceof RadixTransferrableTokensParticle) {
+        return byteIntervalInAtom(particle, atom)
+    } else {
+        return (key: string) => {
+            if (key !== 'serializer') { return { startsAtByte: 0, byteCount: 0 } }
+            return byteIntervalInAtom(particle, atom)(key)
+        }
     }
 }
 
+/*
+* Finds where a DSON encoded particle starts, and it's length, in a DSON encoded atom
+* byte array.
+*/
+const intervalByString = (particleDSON: Buffer, atomDSON: Buffer): ByteInterval => {
+    return {
+        startsAtByte: atomDSON.toString('hex').indexOf(particleDSON.toString('hex')) / 2,
+        byteCount: particleDSON.length,
+    }
+}
+
+/*
+* Finds the byte interval of a key value of a particle, in an atom.
+*
+* For example, it could look for the particle's 'address' key, and it would
+* return where that value starts in the whole atom's byte array, and how many bytes
+* it is. 
+*/
 const byteIntervalInAtom = (particle: RadixParticle, atom: RadixAtom) => (key: string): ByteInterval => {
     const particleEncoded = particle.toDSON()
     const atomEncoded = atom.toDSON()
